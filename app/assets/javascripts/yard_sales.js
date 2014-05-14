35a7ccab25861d8fec92c9449a1fb6c20bb1c622
Runtheball <rjeisenbart@gmail.com>
2014-05-14 08:41:15 -0400

    now with JavaScript goodness

--- app/assets/javascripts/yard_sales.js
+++ app/assets/javascripts/yard_sales.js
@@ -3,3 +3,47 @@ $(function(){
     $(this).hide( "slow" );
   });
 });
+
+$(function(){
+  $(".wow-form").mouseenter(function(){
+    $(this).toggleClass( "framed" );
+  });
+});
+
+$(function(){
+  $(".new_sale_form").on("submit", function(){
+    if (this.text_field) input = " ";
+      $( "form").submit(function( event ) {
+      event.preventDefault();
+    });
+  });
+});
+
+$(function(){
+  $('.flip').click(function () {
+    $(this).find('.card').addClass('flipped').mouseleave(function () {
+        $(this).removeClass('flipped');
+    });
+    return false;
+  });
+});
+
+// $(function(){
+//   $('.howdy')
+//     .on("mouseover",(function() {
+//       $('.bubble').show("slow");
+//     }));
+//     .on("mouseout",(function() {
+//       $('.bubble').removeClass("show");
+//     }));
+// });
+
+$(function(){
+  $('.howdy')
+    .mouseenter(function() {
+      $('.bubble').show("slow");
+    })
+    .mouseleave(function() {
+      $('.bubble').hide("slow");
+    });
+});
